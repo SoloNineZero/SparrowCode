66020e27803da079909764d1e16cfa294fc0cc8d
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-24 18:49:04 +0500

    Initial Commit

---
+++ SparrowCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowCode
+//
+//  Created by Игорь Солодянкин on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
